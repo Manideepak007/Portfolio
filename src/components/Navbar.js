9d8f20bc2091ab4a02fb0b586f4995ec2ee0ff01
Manideepak007 <manideepakgollapudi29@gmail.com>
2024-12-09 23:15:50 +0530

    third commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -75,7 +75,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/Manideepak007/Portfolio.git"
                 target="_blank"
                 className="fork-btn-inner"
               >
